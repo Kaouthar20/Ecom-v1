64be97d241204cc6a71cd20d2533f41708be444e
kaouthar <kaouthar.elouatri@gmail.com>
2020-06-19 12:21:35 +0000

    adding microservice annotation

--- src/main/java/org/sid/ecomv1/dao/CategoryRepository.java
+++ src/main/java/org/sid/ecomv1/dao/CategoryRepository.java
@@ -3,6 +3,8 @@ package org.sid.ecomv1.dao;
 import org.sid.ecomv1.entities.Category;
 import org.sid.ecomv1.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface CategoryRepository extends JpaRepository <Category,Long> {
 }
--- src/main/java/org/sid/ecomv1/dao/ProductRepository.java
+++ src/main/java/org/sid/ecomv1/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package org.sid.ecomv1.dao;
 
 import org.sid.ecomv1.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ProductRepository extends JpaRepository <Product,Long> {
 }
